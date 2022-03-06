e5ca381f81960df098629a7dff7b52e43deb96ca
Ian Lessa <ian.lessa1@gmail.com>
2022-03-06 15:16:24 +0100

    adicionado script para calculo de imc

---
+++ imc.php
@@ -0,0 +1,22 @@
+<?php
+
+if ($argc !== 3) {
+    echo "--- CALCULO DE IMC ---" . PHP_EOL;
+    echo "Uso: " . PHP_EOL;
+    echo "php imc.php <peso em gramas>  <altura em centimetros>" . PHP_EOL . PHP_EOL;
+    echo "Exemplo: " . PHP_EOL;
+    echo "php imc.php 9560 180" . PHP_EOL;
+    die();
+}
+
+$peso = ($argv[1] ?? 1) / 100;
+$altura = ($argv[2] ?? 1) / 100;
+
+$imc = $peso / pow($altura,2);
+
+echo sprintf(
+    "Peso em KG: %.2f\nAltura em M: %.2f\nIMC: %.2f",
+    $peso,
+    $altura,
+    $imc
+);
